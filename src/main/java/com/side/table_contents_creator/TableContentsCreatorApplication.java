299f1f43c52eddda956a154596363b4d7521b7cc
이승혜 <siznorias@gmail.com>
2022-08-01 18:07:56 +0900

    - first commit

---
+++ src/main/java/com/side/table_contents_creator/TableContentsCreatorApplication.java
@@ -0,0 +1,13 @@
+package com.side.table_contents_creator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableContentsCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableContentsCreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/side/table_contents_creator/TableContentsCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.table_contents_creator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableContentsCreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
